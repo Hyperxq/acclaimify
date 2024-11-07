c109f76ba0c3a01f4071915c4cb540717c6cade2
Daniel Ramírez Barrientos <barrientosd9@gmail.com>
2024-11-07 17:28:13 -0600

    feat: allowing deploy for cloudflare

--- apps/applaudify/vite.config.ts
+++ apps/applaudify/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
 
   const urlMap =  {
     production: '/applaudify/',
-    cloudflare: './'
+    cloudflare: './dist/apps/applaudify'
   }
 
   return {
