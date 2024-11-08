cbd77a5053f3a7e4ff4efb9ba4e61658a83ab3f8
Daniel Ramírez Barrientos <barrientosd9@gmail.com>
2024-11-08 16:00:31 -0600

    fix: fixing spa apps on host

--- apps/applaudify/src/main.tsx
+++ apps/applaudify/src/main.tsx
@@ -3,8 +3,11 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from 'react-router-dom';
 
+const isCloudflare = import.meta.env.VITE_DEPLOY_TARGET === 'cloudflare';
+const basename = isCloudflare ? '/' : '/applaudify/';
+
 REACTDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <BrowserRouter basename={basename}>
     <App />
   </BrowserRouter>
 );
