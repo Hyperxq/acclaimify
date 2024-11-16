8e3800cc12a8583207974622752cb62012b0d79f
Daniel Ramírez Barrientos <barrientosd9@gmail.com>
2024-11-15 18:10:14 -0600

    feat: adding fonts

--- apps/frontend/src/components/MainContainer/MainContainer.tsx
+++ apps/frontend/src/components/MainContainer/MainContainer.tsx
@@ -5,8 +5,10 @@ interface Props extends IProps {
 }
 
 export const MainContainer = ({ children, grid = "grid grid-cols-min-content-2  gap-[48.21px] items-center justify-items-center content-center justify-center" }: Props) => {
-  const classes = `${grid} w-screen h-screen bg-cover bg-center bg-no-repeat bg-[url('./assets/blur.png')]`
-  return (<main id="mainContainer" className={classes}>
-    {children}
-  </main>)
+  const classes = `${grid} w-screen h-screen bg-cover bg-center bg-no-repeat bg-[url('./assets/images/blur.png')]`
+  return (
+    <main id="mainContainer" className={classes}>
+      {children}
+    </main>
+  )
 }
